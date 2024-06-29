c50474c47393fc230db5b21d97b1222e41ee35d2
Krisna VJ <vjkrisna995@gmail.com>
2024-06-29 17:54:39 +0530

    Create SingleNumber.java

---
+++ SingleNumber.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int check = 0;
+        for(int ele : arr){
+            check^=ele;         //x^x=0 x^0=x 
+        }
+        return check;
+    }
+}
