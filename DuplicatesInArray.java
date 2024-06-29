60bd364a713b3632612c84befc4b68b11a26c4c4
Krisna VJ <vjkrisna995@gmail.com>
2024-06-29 21:54:10 +0530

    Create DuplicatesInArray.java

---
+++ DuplicatesInArray.java
@@ -0,0 +1,14 @@
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        int[] copy = new int[nums.length+1];
+
+        for(int ele : nums){
+            if(copy[ele]!=0)
+                list.add(ele);
+            else
+                copy[ele] = ele;
+        }
+        return list;
+    }
+}
