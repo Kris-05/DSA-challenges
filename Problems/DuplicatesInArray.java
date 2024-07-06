b0f3a7c54d535c83250be3251adb7bf93d4a2a41
Krisna VJ <vjkrisna995@gmail.com>
2024-07-06 17:36:24 +0530

    Update DuplicatesInArray.java

--- Problems/DuplicatesInArray.java
+++ Problems/DuplicatesInArray.java
@@ -2,10 +2,10 @@
 class Solution {
     public List<Integer> findDuplicates(int[] nums) {
         List<Integer> list = new ArrayList<>();
-        int[] copy = new int[nums.length+1];
+        int[] copy = new int[nums.length+1];        // copy[n] = {0};
 
         for(int ele : nums){
-            if(copy[ele]!=0)
+            if(copy[ele]!=0)        // checkking the index of element in nums is 0 in copy
                 list.add(ele);
             else
                 copy[ele] = ele;
