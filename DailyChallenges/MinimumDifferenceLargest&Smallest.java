63d09cef04a8e440d9b34a7d75df0b882df152a5
Krisna VJ <vjkrisna995@gmail.com>
2024-07-06 17:39:20 +0530

    Update MinimumDifferenceLargest&Smallest.java

--- DailyChallenges/MinimumDifferenceLargest&Smallest.java
+++ DailyChallenges/MinimumDifferenceLargest&Smallest.java
@@ -1,9 +1,10 @@
+// Minimum Difference Between Largest and Smallest Value in Three Moves
 class Solution {
     public int minDifference(int[] nums) {
         int n = nums.length;
         if(n <= 4)
             return 0;
-        Arrays.sort(nums);
+        Arrays.sort(nums);    // sorting makes easier
 
         int k = n-3;        //to check to min possible difference i.e last 3 elements
         int diff = nums[n-1] - nums[0];
