52d79b52239797c4babba321e26c29970957a62a
Krisna VJ <vjkrisna995@gmail.com>
2024-07-06 17:45:06 +0530

    Update ScoreOfString.java

--- DailyChallenges/ScoreOfString.java
+++ DailyChallenges/ScoreOfString.java
@@ -1,3 +1,7 @@
+// Score of a String
+/*
+You are given a string s. The score of a string is defined as the sum of the absolute difference between the ASCII values of adjacent characters.
+*/
 class Solution {
     public int scoreOfString(String s) {
         char[] arr = s.toCharArray();
