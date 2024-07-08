91292e6afaeab843b0b3ed2d40194e942b6dde07
Krisna VJ <vjkrisna995@gmail.com>
2024-07-08 13:39:51 +0530

    Update DuplicateNumber.java

--- Problems/DuplicateNumber.java
+++ Problems/DuplicateNumber.java
@@ -1,12 +1,14 @@
 // Find the Duplicate Number
 class Solution {
     public int findDuplicate(int[] nums) {
+        //A hashset to store unique numbers
         Set<Integer> set = new HashSet();
+        
         for(int i=0;i<nums.length;i++) {
             if(set.contains(nums[i]))
-              return nums[i];
+              return nums[i];            //to find duplicate
             else
-              set.add(nums[i]);
+              set.add(nums[i]);          //adding to hashset
         }
         return -1;
     }
