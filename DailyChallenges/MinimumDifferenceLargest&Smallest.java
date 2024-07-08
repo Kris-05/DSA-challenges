c0e5e5d3851ea4d34b566d61beb077eadb0ca483
Krisna VJ <vjkrisna995@gmail.com>
2024-07-08 13:47:36 +0530

    Update MinimumDifferenceLargest&Smallest.java

--- DailyChallenges/MinimumDifferenceLargest&Smallest.java
+++ DailyChallenges/MinimumDifferenceLargest&Smallest.java
@@ -6,12 +6,20 @@ class Solution {
             return 0;
         Arrays.sort(nums);    // sorting makes easier
 
-        int k = n-3;        //to check to min possible difference i.e last 3 elements
+        // int k = n-3;        //to check to min possible difference i.e last 3 elements
+        // int diff = nums[n-1] - nums[0];
+        // for(int i=k-1;i < n;i++)
+        //     diff = Math.min(diff, nums[i] - nums[i-k+1]);
+        
+        //<or>
+        
+        int i,j;            //to check to min possible difference i.e last 3 elements
         int diff = nums[n-1] - nums[0];
 
-        for(int i=k-1;i < n;i++){
-            diff = Math.min(diff, nums[i] - nums[i-k+1]);
+        for(i=n-4,j=0;i < n;i++,j++){
+            diff = Math.min(diff, nums[i] - nums[j]);   // maximum difference from first3 & last3
         }
+        
         return diff;
     }
 }
